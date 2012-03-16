76e0457e5498f8738f40ddb6014b6698b4f65b36
Paul Gambill <pgambill@ubermind.com>
2012-03-16 11:09:47 -0700

    Added GraphViewController

---
+++ GraphViewController.m
@@ -0,0 +1,43 @@
+//
+//  GraphViewController.m
+//  Calculator
+//
+//  Created by Paul Gambill on 3/16/12.
+//  Copyright (c) 2012 Deloitte | Übermind. All rights reserved.
+//
+
+#import "GraphViewController.h"
+
+@interface GraphViewController ()
+
+@end
+
+@implementation GraphViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
