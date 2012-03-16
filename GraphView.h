1615f973874fcd2a8bbc7821109643e989a0e622
Paul Gambill <pgambill@ubermind.com>
2012-03-16 11:13:04 -0700

    Added GraphView

---
+++ GraphView.h
@@ -0,0 +1,13 @@
+//
+//  GraphView.h
+//  Calculator
+//
+//  Created by Paul Gambill on 3/16/12.
+//  Copyright (c) 2012 Deloitte | Übermind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphView : UIView
+
+@end
---
+++ GraphView.m
@@ -0,0 +1,31 @@
+//
+//  GraphView.m
+//  Calculator
+//
+//  Created by Paul Gambill on 3/16/12.
+//  Copyright (c) 2012 Deloitte | Übermind. All rights reserved.
+//
+
+#import "GraphView.h"
+
+@implementation GraphView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
