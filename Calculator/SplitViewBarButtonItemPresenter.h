40bbaff9122abe0a092b106bfd884d3896551a97
Paul Gambill <pgambill@ubermind.com>
2012-04-24 16:15:19 -0700

    added BarButtonItem protocol header file

---
+++ Calculator/SplitViewBarButtonItemPresenter.h
@@ -0,0 +1,13 @@
+//
+//  SplitViewBarButtonItemPresenter.h
+//  Calculator
+//
+//  Created by Paul Gambill on 4/24/12.
+//  Copyright (c) 2012 Deloitte | Übermind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol SplitViewBarButtonItemPresenter <NSObject>
+@property (nonatomic, strong) UIBarButtonItem *splitviewBarButtonItem;
+@end
